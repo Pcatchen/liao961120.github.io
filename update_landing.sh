c3ab558f5b650898934c6eab4129942dcc99e33a
liao <liaomovie2@gmail.com>
2018-12-15 21:40:32 +0800

    up

--- update_landing.sh
+++ update_landing.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
+cd Hugo-landing
+
+cp -r docs/css docs-min/
+cp -r docs/css docs-min/
+cp -r docs/img docs-min/
+cp -r docs/js docs-min/
+cp -r docs/project docs-min/
+cp -r docs/styles.css docs-min/
+cp -r docs/index.html docs-min/
+
+cd ..
+
 cp -r Hugo-landing/docs-min/* .
