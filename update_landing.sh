bfa577fcaea233396edf5c174beda7eb010d3614
liao <liaomovie2@gmail.com>
2019-01-22 17:47:14 +0800

    rm project pages

--- update_landing.sh
+++ update_landing.sh
@@ -7,7 +7,11 @@ cp -r docs/css docs-min/
 cp -r docs/css docs-min/
 cp -r docs/img docs-min/
 cp -r docs/js docs-min/
-#cp -r docs/project docs-min/
+
+rm -r docs/project/**/index.html
+rm -r docs/project/index.html docs/project/index.xml
+cp -r docs/project docs-min/
+
 cp -r docs/styles.css docs-min/
 cp -r docs/index.html docs-min/
 cp -r docs/site.webmanifest docs-min/
