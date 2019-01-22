92336da517a64a8d78472c8ba5d915728b9e0c29
liao <liaomovie2@gmail.com>
2019-01-22 17:58:50 +0800

    up

--- update_landing.sh
+++ update_landing.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
-cd Hugo-landing
 
+# Clean up old landing page dependencies
+rm -r project/*
+
+
+# Build Landing Page
+cd Hugo-landing
 Rscript -e 'blogdown::build_site()'
 
 cp -r docs/css docs-min/
@@ -8,14 +13,19 @@ cp -r docs/css docs-min/
 cp -r docs/img docs-min/
 cp -r docs/js docs-min/
 
-rm -r docs/project/**/index.html
-rm -r docs/project/index.html docs/project/index.xml
+rm -r docs/project/*/*.html
+rm -r docs/project/index*
 cp -r docs/project docs-min/
 
 cp -r docs/styles.css docs-min/
 cp -r docs/index.html docs-min/
 cp -r docs/site.webmanifest docs-min/
 
+cp -r docs-min/* ..
+
 cd ..
 
-cp -r Hugo-landing/docs-min/* .
+
+# Clean up
+rm -r Hugo-landing/docs/*
+rm -r Hugo-landing/docs-min/*
